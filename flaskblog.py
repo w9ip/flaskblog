eb6bf2dcf5fb3a50a14bcf857faaf9f6a93c13d8
w9i <w9i@w9is-MacBook.local>
2023-07-30 02:12:25 +0300

    Video 2.9

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '37c0537c817855d926550fcc07bb84b6'
 
 posts = [
     {
@@ -29,6 +31,20 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
